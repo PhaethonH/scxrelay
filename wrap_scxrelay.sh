c829564269d39c82679f0bf9382792c7c7fb9dd0
Phaethon H <PhaethonH@gmail.com>
2018-12-12 14:10:42 -0800

    Add a short delay before launching wrapped binary to allow udev devices created by uinput to settle.

--- wrap_scxrelay.sh
+++ wrap_scxrelay.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Wrapper around scxrelay to automatically find and use (first) Steam Controller virtual xpad device.
-# This script is public domain.
+# Wrapper around scxrelay to automatically find and use (first) Steam Controller virtual xpad device while game is running.
+# This script is in the public domain.
 
 # Path to SteamController Xpad Relay.
-SCXRELAY=scxrelay
+SCXRELAY=$(PATH=.:"$PATH" which scxrelay || which scxrelay.x86)
 
 # Paths to system utilities.
 UDEVADM=udevadm
@@ -20,18 +20,19 @@ RELAYPID=0
 for evdev in "$EVENT_PREFIX"*; do
   # Search for the two lines "P: [...]/virtual/[...]" and "E: ID_INPUT_JOYSTICK=1", which are characteristic of the Steam Controller virtual xpad device.
   if [ `$UDEVADM info -n "$evdev" | $GREP -c '^\(P: .*/virtual/\|E: ID_INPUT_JOYSTICK=1\)'` = 2 ]; then
-    # match
     if [ x"$EVENT_PATH" = "x" ]; then
-      EVENT_PATH="$evdev"
+      EVENT_PATH="$evdev"  # store first match.
     fi
   fi
 done
 
 # Start up the relay in background.
-$SCXRELAY "$EVENT_PATH" "$UINPUT_PATH" &
-RELAYPID=$!
-# Short sleep for udev to settle.
-sleep 0.5
+if [ x"$EVENT_PATH" != "x" ]; then
+  $SCXRELAY "$EVENT_PATH" "$UINPUT_PATH" &
+  RELAYPID=$!
+  # Short sleep for udev to settle.
+  sleep 0.5
+fi
 
 # Run rest of command line (e.g. the actual game)
 "$@"
